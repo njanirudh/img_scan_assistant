3bc675424cea797204a0cc06870c6f96afa5cfb4
njanirudh <anijaya9@gmail.com>
2021-07-06 12:07:21 +0200

    Refactered code

--- setup.py
+++ setup.py
@@ -3,10 +3,9 @@ from setuptools import setup
 setup(
     name='ImageScanAssistant ',
     version='1.0',
-    packages=[''],
     url='https://github.com/njanirudh/img_scan_assistant',
     license='MIT',
-    author='anirudh',
+    author='njanirudh',
     author_email='anijaya9@gmail.com',
     description='Commandline application for cropping individual images from a large scanned image.'
 )
